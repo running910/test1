7da21c0b416bea03d53790dd9a0ace6b5f7a9f05
Zhixing Chen <running910@gmail.com>
2024-04-07 15:31:29 +0800

    test1

---
+++ demo/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/running910/test1/test1"
+
+func main() {
+	test1.Hello()
+}
---
+++ test1/test1.go
@@ -0,0 +1,9 @@
+package test1
+
+import (
+	"github.com/running910/gokit/logger"
+)
+
+func Hello() {
+	logger.Info("hello from test1")
+}
